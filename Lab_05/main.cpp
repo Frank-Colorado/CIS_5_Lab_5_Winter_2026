bffc758587fd5790de3c0f6d04c1f6e8b3d743de
Frank Colorado <chawnkiee@gmail.com>
2026-02-09 17:14:35 -0800

    created structure with constructor
    created a structure for company's business cards with a constructor

--- Lab_05/main.cpp
+++ Lab_05/main.cpp
@@ -5,6 +5,21 @@
 
 using namespace std;
 
+// Structure for a company's business card
+struct BusinessCard {
+	string name;
+	string address;
+	int phoneNumber;
+	string workingHours;
+	string representative;
+	double quotedPrice;
+
+	// Constructor for the BusinessCard structure
+	BusinessCard(const string& n, const string& a, int p, const string& w, const string& r, double q)
+		: name(n), address(a), phoneNumber(p), workingHours(w), representative(r), quotedPrice(q) {}
+
+};
+
 int main () {
 
 
