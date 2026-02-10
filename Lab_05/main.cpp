4b40b9bc4a3e1b94f47e440ebe3b693421119c74
Frank Colorado <chawnkiee@gmail.com>
2026-02-09 17:19:43 -0800

    Used constructor in main
    used the constructor to create 5 different companies

--- Lab_05/main.cpp
+++ Lab_05/main.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 #include <ctime>
 #include <string>
+#include <iomanip>
 
 using namespace std;
 
@@ -21,6 +22,17 @@ struct BusinessCard {
 };
 
 int main () {
+	// Seed the random number generator
+	srand(time(nullptr));
+
+	// Business cards for 5 different companies
+	// The quoted price is randomly generated between 1000 and 5000
+	BusinessCard company1("Tech Solutions", "123 Tech Street", 1234567890, "9 AM - 5 PM", "Alice Johnson", 1000 + rand() % 4000 + 1);
+	BusinessCard company2("Creative Designs", "456 Design Avenue", 9876543210, "10 AM - 6 PM", "Bob Smith", 1000 + rand() % 4000 + 1);
+	BusinessCard company3("Green Energy", "789 Green Road", 5555555555, "8 AM - 4 PM", "Charlie Brown", 1000 + rand() % 4000 + 1);
+	BusinessCard company4("Health Plus", "321 Health Blvd", 1112223333, "7 AM - 3 PM", "Diana Prince", 1000 + rand() % 4000 + 1);
+	BusinessCard company5("Finance Experts", "654 Finance Lane", 4443332222, "9 AM - 5 PM", "Edward Norton", 1000 + rand() % 4000 + 1);
+
 
 
 
